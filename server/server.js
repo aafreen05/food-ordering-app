60f86cbb7822ecd41eb59d9b0806d450d611b090
Aafreen <aafreenbegam2007@gmail.com>
2026-09-06 21:27:30 +0530

    Use Render provided port

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use("/api/foods", foodRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
